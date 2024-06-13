c80ea2d24fce2c407c05a01575161d1c97fd8db5
Kristina Stasilevich <kristina.stasilevich@gmail.com>
2024-06-13 20:23:22 +0300

    Add grid homework

--- MySeleniumProject/src/test/java/selenide/TestBase.java
+++ MySeleniumProject/src/test/java/selenide/TestBase.java
@@ -5,6 +5,8 @@ import com.codeborne.selenide.Selenide;
 import com.epam.reportportal.service.ReportPortal;
 import io.qameta.allure.Allure;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,14 +16,19 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Calendar;
 
-import static com.codeborne.selenide.Browsers.CHROME;
-import static com.codeborne.selenide.Browsers.EDGE;
+import static com.codeborne.selenide.Browsers.*;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
     @BeforeMethod
     protected void setup() {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setBrowserName(CHROME);
+            caps.setPlatform(Platform.WINDOWS);
+
+            Configuration.remote = "http://192.168.137.1:4444/wd/hub";
             Configuration.browser = CHROME;
+            Configuration.browserCapabilities = caps;
             Configuration.pageLoadTimeout = 5000;
             Configuration.browserSize = "1920x1080";
             Configuration.baseUrl = "https://litecart.stqa.ru/en/";
--- MySeleniumProject/src/test/java/staticpageobject/TestBase.java
+++ MySeleniumProject/src/test/java/staticpageobject/TestBase.java
@@ -1,23 +1,45 @@
 package staticpageobject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.Browsers.EDGE;
 
 public class TestBase {
     protected WebDriver driver;
 
     @BeforeMethod
-    protected void setup() {
-        Browser browser = Browser.valueOf(System.getProperty("browser","chrome")) ;
+    protected void setup() throws MalformedURLException {
+        Browser browser = Browser.valueOf(System.getProperty("browser","chrome"));
 
-        driver = switch(browser) {
-            case chrome -> new ChromeDriver();
-            case edge -> new EdgeDriver();
-        };
+        String platform = System.getProperty("os", "windows");
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        switch(browser) {
+            case chrome -> caps.setBrowserName(CHROME);
+            case edge -> caps.setBrowserName(EDGE);
+        }
+
+        switch(platform) {
+            case "windows" -> caps.setPlatform(Platform.WINDOWS);
+            case "linux" -> caps.setPlatform(Platform.LINUX);
+        }
+
+        //caps.setPlatform(Platform.WINDOWS);
+
+        driver = new RemoteWebDriver(new URL("http://192.168.137.1:4444/wd/hub"), caps);
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
